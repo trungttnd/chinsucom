ca8d62c0c7af19eb785563cad8b1ba432706d5a6
Trần Thành Trung <trungtt@comartek.com>
2023-02-17 19:52:26 +0700

    reducejs

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,10 @@ import NoneSSRWrapper from "../components/NoneSSRWrapper";
 import images from "../utils/const";
 import { shimmer, toBase64 } from "../utils/image_data";
 
+export const config = {
+  unstable_runtimeJS: false,
+};
+
 const Home: NextPage = () => {
   return (
     <>
@@ -19,13 +23,17 @@ const Home: NextPage = () => {
       <VideoList />
       {/* FOOTER BANNER */}
       <div style={{ width: "100%" }}>
-        <Image className="footer_banner" 
-        src={images.homepage.bottom} 
-        width="1920"
-        height="594"
-        placeholder="blur" 
-        blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
-        alt="" />
+        <Image
+          className="footer_banner"
+          src={images.homepage.bottom}
+          width="1920"
+          height="594"
+          placeholder="blur"
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(
+            shimmer(700, 475)
+          )}`}
+          alt=""
+        />
       </div>
       <Subcribe />
     </>
